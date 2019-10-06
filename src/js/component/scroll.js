e17ae0b2f3aa2869d12331bcc81aaa36e5465587
vadimbelinovich <vadimbelinovich@gmail.com>
2019-10-06 21:35:35 +0300

    edited

--- src/js/component/scroll.js
+++ src/js/component/scroll.js
@@ -1,12 +1,12 @@
-const checkInput = document.querySelector('#menu-switch');
-const wrapper = document.querySelector('.wrapper');
+const checkInput = $('#menu-switch');
+const wrapper = $('.wrapper');
 
 function check() {
-  if(checkInput.checked === true) {
-    wrapper.classList.add('no-scroll');
-  } else {
-    wrapper.classList.remove('no-scroll');
-  }
+  $(checkInput).on('click', function(){
+    if(checkInput.checked = true) {
+      wrapper.toggleClass('no-scroll');
+    } 
+  })
 }
 
 export default check();

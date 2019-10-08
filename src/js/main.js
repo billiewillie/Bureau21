eb4ea53b6c749c420917b82f665980259d3f268f
vadimbelinovich <vadimbelinovich@gmail.com>
2019-10-08 18:56:09 +0300

    edited

--- src/js/main.js
+++ src/js/main.js
@@ -3,18 +3,7 @@ import "lightbox2";
 import check from "./component/scroll";
 import tab from "./component/tab";
 
-$("#slider-1").slick({
-  dots: true,
-  arrows: true,
-  infinite: true,
-  autoplay: false,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  autoplaySpeed: 4000,
-  mobileFirst: true,
-});
-
-$("#slider-2").slick({
+$(".slider").slick({
   dots: true,
   arrows: true,
   infinite: true,

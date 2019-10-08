5796032ba4e4974c2fce1fed44faa2939faa2022
vadimbelinovich <vadimbelinovich@gmail.com>
2019-10-08 14:17:04 +0300

    edited

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,26 @@
+import "slick-carousel";
+import "lightbox2";
 import check from "./component/scroll";
 import tab from "./component/tab";
+
+$("#slider-1").slick({
+  dots: true,
+  arrows: true,
+  infinite: true,
+  autoplay: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplaySpeed: 4000,
+  mobileFirst: true,
+});
+
+$("#slider-2").slick({
+  dots: true,
+  arrows: true,
+  infinite: true,
+  autoplay: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplaySpeed: 4000,
+  mobileFirst: true,
+});

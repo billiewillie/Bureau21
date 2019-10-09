df7816cf699b6528397cdbdb15cf616aa911dcd3
vadimbelinovich <vadimbelinovich@gmail.com>
2019-10-09 14:14:59 +0300

    edited

--- src/php/mail.php
+++ src/php/mail.php
@@ -45,7 +45,7 @@ function adopt($text) {
 
 $headers = "MIME-Version: 1.0" . PHP_EOL .
 "Content-Type: text/html; charset=utf-8" . PHP_EOL .
-'From: '.adopt($project_name).' <v.belinovich@uno-agency.ru>' . PHP_EOL .
-'Reply-To: v.belinovich@uno-agency.ru' . PHP_EOL;
+'From: '.adopt($project_name).' <office@21.spb.ru>' . PHP_EOL .
+'Reply-To: office@21.spb.ru' . PHP_EOL;
 
 mail($admin_email, adopt($form_subject), $message, $headers );
